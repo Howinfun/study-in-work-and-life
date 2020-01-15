9edfc329221f6102b70250b708e6536614f289b0
Howinfun <876237770@qq.com>
2020-01-15 17:58:52 +0800

    redis例子

--- src/main/java/com/hyf/BootApplication.java
+++ src/main/java/com/hyf/BootApplication.java
@@ -1,13 +1,17 @@
-package com.hyf.algorithm;
+package com.hyf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Howinfun
+ * @desc
+ * @date 2020/1/15
+ */
 @SpringBootApplication
-public class TestAlgorithmApplication {
+public class BootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestAlgorithmApplication.class, args);
+        SpringApplication.run(BootApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/hyf/originalArticles/redis/TestController.java
@@ -0,0 +1,25 @@
+package com.hyf.originalArticles.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Howinfun
+ * @desc
+ * @date 2020/1/15
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping
+    public String test(){
+
+        System.out.println(redisTemplate.getConnectionFactory());
+        return "redis";
+    }
+}
