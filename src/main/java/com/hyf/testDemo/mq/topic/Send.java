9afb1e1ef32f919d2b16b4b1ac917aa0ae870506
Howinfun <876237770@qq.com>
2020-07-06 17:58:22 +0800

    rabbitmq:topic

---
+++ src/main/java/com/hyf/testDemo/mq/topic/Send.java
@@ -0,0 +1,19 @@
+package com.hyf.testDemo.mq.topic;
+
+/**
+ * @author Howinfun
+ * @desc
+ * @date 2020/7/6
+ */
+public class Send {
+
+    private static final String exchangeName = "hyf.topic.exchange";
+    private static final String routingkeyByLogin = "user.login.info";
+    private static final String routingkeyByRegister = "user.register.info";
+    private static final String routingkeyByOrder = "order.detail.info";
+    private static final String routingkeyByStock = "stock.detail.info";
+
+    public static void main(String[] args) {
+
+    }
+}
