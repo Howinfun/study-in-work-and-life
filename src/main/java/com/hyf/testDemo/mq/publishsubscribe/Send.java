248220b0c1372481a9564c38f8626c531aca7286
Howinfun <876237770@qq.com>
2020-07-07 14:02:14 +0800

    rabbitmq:rpc

--- src/main/java/com/hyf/testDemo/mq/publishsubscribe/Send.java
+++ src/main/java/com/hyf/testDemo/mq/publishsubscribe/Send.java
@@ -21,10 +21,11 @@ public class Send {
         ConnectionFactory factory = ConnectionFactoryUtils.getFactory();
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()){
-            // fanout 类型的交换器
+            // 声明 fanout 类型的交换器
             channel.exchangeDeclare(exchangeName,"fanout");
             for (int i = 0; i <= 10; i++){
                 String message = "消息"+i;
+                // 直接指定交换器进行消息发布
                 channel.basicPublish(exchangeName,"", null, message.getBytes());
             }
         }
