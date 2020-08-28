65e5add4622da975f36ed6b62c2c19c86617ba97
Howinfun <876237770@qq.com>
2020-08-28 09:19:05 +0800

    zookeeper getChildren

--- src/main/java/com/hyf/testDemo/zk/crud/CURDDemo.java
+++ src/main/java/com/hyf/testDemo/zk/crud/CURDDemo.java
@@ -22,9 +22,10 @@ public class CURDDemo {
 
 
         client.start();
-        //create("/test/child2","child1");
+        //create("/test/child4","child4");
         //delete("/test/child3");
-        update("/test/child1","child1child1");
+        //("/test/child1","child1child1");
+        getChildren("/test");
         client.close();
     }
 
@@ -51,6 +52,17 @@ public class CURDDemo {
         log.info("{} 节点更新数据成功：更新前数据为 {}，更新后数据为 {}",path,oldData,new String(data));
     }
 
+    public static String getData(String path) throws Exception{
+
+        Stat stat = client.checkExists().forPath(path);
+        if (null == stat){
+            log.info("{} 节点不存在！",path);
+            return null;
+        }else {
+            return new String(client.getData().forPath(path));
+        }
+    }
+
     public static void delete(String path) throws Exception{
 
         Stat stat = client.checkExists().forPath(path);
@@ -65,6 +77,31 @@ public class CURDDemo {
         }else {
             log.info("{} 节点存在子节点，不能删除！",path);
         }
+    }
 
+    public static void getChildren(String path) throws Exception{
+
+        Stat stat = client.checkExists().forPath(path);
+        if (null == stat){
+            log.info("{} 节点不存在！",path);
+            return;
+        }
+        List<String> childList = client.getChildren().forPath(path);
+        if (childList.isEmpty()){
+            log.info("{} 节点不存在子节点！",path);
+        }else {
+            // child 是子节点的名称，不是全路径
+            childList.forEach(child -> {
+                String childPath = path + "/" + child;
+                String data  = null;
+                try {
+                    data = getData(childPath);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                log.info("子节点 {} 的数据为：{}",childPath,data);
+            });
+
+        }
     }
 }
