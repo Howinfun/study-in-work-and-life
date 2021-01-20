d49a74f9db06da206a46a543d7759e7a54fb02fa
heyongfeng <heyongfeng@yy.com>
2021-01-20 18:47:56 +0800

    todo

--- dubbo/dubbo-consumer/src/main/java/com/winfun/controller/HelloController.java
+++ dubbo/dubbo-consumer/src/main/java/com/winfun/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.winfun.controller;
 
+import com.alibaba.csp.sentinel.EntryType;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.winfun.entity.pojo.ApiResult;
 import com.winfun.service.DubboServiceOne;
 import org.apache.dubbo.config.annotation.DubboReference;
@@ -17,12 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @DubboReference(check = false,lazy = true,mock = "return null",retries = 1)
+    @DubboReference(check = false,lazy = true,retries = 1)
     private DubboServiceOne dubboServiceOne;
 
     @GetMapping("/{name}")
     public ApiResult sayHello(@PathVariable("name") String name){
-        String hello = dubboServiceOne.sayHello(name);
+        System.out.println("有东西进来了");
+        String hello = this.sayHelloByDubbo(name);
         return ApiResult.success(hello);
+    }
+
+    @SentinelResource(value = "sayHello",entryType = EntryType.IN)
+    private String sayHelloByDubbo(String name){
+        String hello = dubboServiceOne.sayHello(name);
+        return hello;
     }
 }
--- dubbo/dubbo-consumer/src/test/java/com/winfun/serviceone/ServiceOneApplicationTests.java
+++ dubbo/dubbo-consumer/src/test/java/com/winfun/serviceone/ServiceOneApplicationTests.java
@@ -1,13 +1,45 @@
 package com.winfun.serviceone;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRuleManager;
+import com.winfun.DubboServiceApplication;
+import com.winfun.service.DubboServiceOne;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class ServiceOneApplicationTests {
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest(classes = DubboServiceApplication.class)
+@RunWith(SpringRunner.class)
+public class ServiceOneApplicationTests {
+
+    static {
+            List<DegradeRule> rules = new ArrayList<>();
+            DegradeRule rule = new DegradeRule();
+            rule.setResource("com.winfun.service.DubboServiceOne");
+            // set threshold RT, 10 ms
+            rule.setCount(10);
+            rule.setGrade(RuleConstant.DEGRADE_GRADE_RT);
+            rule.setTimeWindow(10);
+            rules.add(rule);
+            DegradeRuleManager.loadRules(rules);
+    }
+
+    @DubboReference
+    private DubboServiceOne dubboServiceOne;
 
 	@Test
-	void contextLoads() {
+    @SentinelResource
+	public void contextLoads() {
+        for (int i = 0; i < 100; i++) {
+            dubboServiceOne.sayHello("winfun");
+        }
 	}
 
 }
