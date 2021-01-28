17163aac0273e049a93806b0a63361a17e2d15ee
heyongfeng <heyongfeng@yy.com>
2021-01-28 10:27:16 +0800

    接入 Sentinel-Dashboard

--- dubbo/dubbo-consumer/src/main/java/com/winfun/DubboServiceApplication.java
+++ dubbo/dubbo-consumer/src/main/java/com/winfun/DubboServiceApplication.java
@@ -7,6 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * 接入 Sentinel-Dashboard，启动项目时需要增加下面的 JVM 参数：
+ * -Djava.net.preferIPv4Stack=true -Dproject.name=dubbo-consumer-demo -Dcsp.sentinel.api.port=8720  -Dcsp.sentinel
+ * .dashboard.server=localhost:8080
+ *
+ * -Dcsp.sentinel.api.port：客户端的 port，用于上报相关信息
+ * -Dcsp.sentinel.dashboard.server：控制台的地址
+ * -Dproject.name：应用名称，会在控制台中显示
+ * -Djava.net.preferIPv4Stack：某些环境下本地运行 Dubbo 服务还需要加上
+ * @author winfun
+ * @date 2021/1/22 2:02 下午
+ **/
 @EnableDubbo
 @ServletComponentScan
 @SpringBootApplication
