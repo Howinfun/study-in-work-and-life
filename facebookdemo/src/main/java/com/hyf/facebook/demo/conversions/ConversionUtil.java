dbb21f5fe23e69e78f4512cda47673dd348097c0
heyongfeng <heyongfeng@yy.com>
2021-02-04 19:03:53 +0800

    conversions_api

--- facebookdemo/src/main/java/com/hyf/facebook/demo/conversions/ConversionUtil.java
+++ facebookdemo/src/main/java/com/hyf/facebook/demo/conversions/ConversionUtil.java
@@ -1,12 +1,16 @@
 package com.hyf.facebook.demo.conversions;
 
 import com.hyf.facebook.demo.contants.HttpConstant;
+import com.hyf.facebook.demo.conversions.dto.FacebookConversionData;
 import com.hyf.facebook.demo.conversions.dto.FacebookConversionRequest;
 import com.hyf.facebook.demo.conversions.dto.FacebookConversionResponse;
+import com.hyf.facebook.demo.conversions.dto.UserData;
 import com.hyf.facebook.demo.utils.http.OkHttpClientUtils;
 import com.hyf.facebook.demo.utils.json.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Response;
+
+import java.util.Collections;
 
 /**
  *
@@ -30,7 +34,7 @@ public class ConversionUtil {
      **/
     public Integer addEvent(String pixelId, String accessToken, FacebookConversionRequest data){
         String url = API + HttpConstant.PATH_PREFIX + API_VERSION + HttpConstant.PATH_PREFIX + pixelId +
-                HttpConstant.PATH_PREFIX + "events?access_token="+accessToken;
+                HttpConstant.PATH_PREFIX + "events";
         try (Response response = OkHttpClientUtils.post(url, data)) {
             if (response.isSuccessful()) {
                 String result = response.body().string();
@@ -47,4 +51,51 @@ public class ConversionUtil {
         return 0;
     }
 
+    public static void main(String[] args) {
+        /**
+         * curl --location --request POST 'https://graph.facebook.com/v9.0/{pixelId}/events' \
+         * --header 'Content-Type: application/json' \
+         * --data-raw '{
+         *     "access_token":"",
+         *     "data": [
+         *         {
+         *             "event_name": "AddToCart",
+         *             "event_time": 1612429570,
+         *             "user_data": {
+         *                 "fn": ""
+         *             }
+         *         }
+         *     ],
+         *     "partner_agent": "",
+         *     "test_event_code": ""
+         * }'
+         */
+        String pixelId = "";
+        String accessToken = "";
+        FacebookConversionRequest request = new FacebookConversionRequest();
+        request.setAccessToken(accessToken);
+        request.setPartnerAgent("");
+        request.setTestEventCode("");
+        FacebookConversionData data = new FacebookConversionData();
+        data.setEventName("AddToCart");
+        data.setEventTime(1612429570L);
+        UserData userData = new UserData();
+        userData.setFn("6a2d68a4d63341d206f2b735f45fd64dd00e5a286ade91ab1c30141590c1cf2c");
+        data.setUserData(userData);
+        request.setData(Collections.singletonList(data));
+        ConversionUtil util = new ConversionUtil();
+        for (int i = 0; i < 50000; i++) {
+            try {
+                Thread.sleep(1000);
+                Integer receivedCount = util.addEvent(pixelId,accessToken,request);
+                if (receivedCount == 0){
+                    break;
+                }
+                log.info("第{}次调用成功，received_count is {}",i,receivedCount);
+            }catch (Exception e){
+                log.error("调用失败！");
+            }
+
+        }
+    }
 }
--- facebookdemo/src/main/java/com/hyf/facebook/demo/conversions/dto/FacebookConversionRequest.java
+++ facebookdemo/src/main/java/com/hyf/facebook/demo/conversions/dto/FacebookConversionRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * 参数：https://developers.facebook.com/docs/marketing-api/conversions-api/parameters
@@ -13,47 +14,14 @@ import java.io.Serializable;
 @Data
 public class FacebookConversionRequest implements Serializable {
 
-    private static final long serialVersionUID = 459158464533612095L;
-    /**
-     * A Facebook pixel Standard Event or Custom Event name. This field is used to deduplicate events sent by both Facebook Pixel and Conversions API. event_id is also used in deduplication.
-     * Required
-     */
-    @JsonProperty("event_name")
-    private String eventName;
-    /**
-     * A Unix timestamp in seconds indicating when the actual event occurred. The specified time may be earlier than
-     * the time you send the event to Facebook. This is to enable batch processing and server performance
-     * optimization. You must send this date in GMT time zone.
-     *
-     * event_time can be up to 7 days before you send an event to Facebook. If any event_time in data is greater than
-     * 7 days in the past, we return an error for the entire request and process no events.
-     * Required
-     */
-    @JsonProperty("event_time")
-    private Long eventTime;
-    @JsonProperty("event_id")
-    private String eventId;
-    /**
-     * The browser URL where the event happened.
-     */
-    @JsonProperty("event_source_url")
-    private String eventSourceUrl;
-    /**
-     * Required
-     */
-    @JsonProperty("user_data")
-    private UserData userData;
-    @JsonProperty("custom_data")
-    private CustomData customData;
-    /**
-     * A flag that indicates we should not use this event for ads delivery optimization. If set to true, we only use the event for attribution.
-     */
-    @JsonProperty("opt_out")
-    private Boolean optOut;
-    /**
-     * This field allows you to specify where your conversions occurred. Knowing where your events took place helps ensure your ads go to the right people.
-     */
-    @JsonProperty("action_source")
-    private String actionSource;
+    private static final long serialVersionUID = 769518124823428751L;
+
+    private List<FacebookConversionData> data;
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("partner_agent")
+    private String partnerAgent;
+    @JsonProperty("test_event_code")
+    private String testEventCode;
 
 }
