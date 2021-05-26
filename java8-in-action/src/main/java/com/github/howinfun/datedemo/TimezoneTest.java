d216ae8be6cdd92d8983ac58364e6d757bbf2a93
heyongfeng <heyongfeng@joyy.sg>
2021-05-26 11:00:50 +0800

    get rawoffset from timezone

--- java8-in-action/src/main/java/com/github/howinfun/datedemo/TimezoneTest.java
+++ java8-in-action/src/main/java/com/github/howinfun/datedemo/TimezoneTest.java
@@ -2,6 +2,8 @@ package com.github.howinfun.datedemo;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.TimeZone;
 
 /**
  *
@@ -10,13 +12,24 @@ import java.time.ZoneId;
  **/
 public class TimezoneTest {
 
+    private static Integer num = 1 * 60 * 60 * 1000;
+
     public static void main(String[] args) {
         /**
          * 根据timezone查询offset
          * Asia/Shanghai  -> +08:00
          */
         String timezone = "Asia/Shanghai";
-        String offset = ZoneId.of(timezone).getRules().getOffset(LocalDateTime.now()).toString();
+        ZoneOffset zoneOffset = ZoneId.of(timezone).getRules().getOffset(LocalDateTime.now());
+        String offset = zoneOffset.toString();
         System.out.println(offset);
+        String timezone2 = "America/Recife";
+        /**
+         * 获取timezone到UTC的时间间隔 ->  mills  -> 小时
+         */
+        TimeZone timeZone = TimeZone.getTimeZone(ZoneId.of(timezone2));
+        int rawOffset = timeZone.getRawOffset();
+        System.out.println(rawOffset/num);
+
     }
 }
