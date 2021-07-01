6f7b64611f258ed8939181ad673692c10d327431
heyongfeng <heyongfeng@yy.com>
2021-07-01 15:18:23 +0800

    自定义componentScan 过滤器

--- spring-in-action/src/main/java/com/github/howinfun/demo/ioc/componentscan/Configuration.java
+++ spring-in-action/src/main/java/com/github/howinfun/demo/ioc/componentscan/Configuration.java
@@ -3,17 +3,25 @@ package com.github.howinfun.demo.ioc.componentscan;
 import com.github.howinfun.demo.ioc.componentscan.color.Color;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author winfun
  * @date 2021/7/1 2:30 下午
  **/
-@ComponentScan(basePackages = "com.github.howinfun.demo.ioc.componentscan.color")
-@ComponentScan(basePackageClasses = {Configuration.class})
+//@ComponentScan(basePackages = "com.github.howinfun.demo.ioc.componentscan.color")
 // @ComponentScans({
 //         @ComponentScan(basePackages = "com.github.howinfun.demo.ioc.componentscan.color"),
 //         @ComponentScan(basePackageClasses = {Configuration.class})
 // })
+@ComponentScan(basePackageClasses = {Configuration.class}
+                                    // 指定类型为Color的组件不注入，包括子类
+                ,excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,value = Color.class),
+                                    // 指定带@Component注解的组件不注入
+                                @ComponentScan.Filter(type = FilterType.ANNOTATION,value = Component.class),
+                                // 如果父类是Color，则不注入
+                                @ComponentScan.Filter(type = FilterType.CUSTOM,value = CustomFilter.class)})
 public class Configuration {
 }
--- spring-in-action/src/main/java/com/github/howinfun/demo/ioc/componentscan/color/Red.java
+++ spring-in-action/src/main/java/com/github/howinfun/demo/ioc/componentscan/color/Red.java
@@ -1,9 +1,12 @@
 package com.github.howinfun.demo.ioc.componentscan.color;
 
+import org.springframework.stereotype.Component;
+
 /**
  * 红色
  * @author winfun
  * @date 2021/7/1 2:25 下午
  **/
+@Component
 public class Red extends Color{
 }
